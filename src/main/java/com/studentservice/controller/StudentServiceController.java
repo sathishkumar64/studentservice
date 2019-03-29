4b8a4d965facada926460c429671e4f70b8a397c
sathish vasu <sathishkumar64@gmail.com>
2019-03-29 14:42:02 +0530

    added exception handling

--- src/main/java/com/studentservice/controller/StudentServiceController.java
+++ src/main/java/com/studentservice/controller/StudentServiceController.java
@@ -16,41 +16,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.studentservice.domain.StudentAppData;
 import com.studentservice.domain.Student;
+import com.studentservice.domain.StudentAppData;
 import com.studentservice.repositories.MongoDBStudentRepository;
 
 @RestController
-@RequestMapping(path="/api")
+@RequestMapping(path = "/api")
 public class StudentServiceController {
-	
-	Logger logger = LoggerFactory.getLogger(this.getClass());	
-	
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	public MongoDBStudentRepository mongoDBStudentRepository;
 
-	
-	@GetMapping(path="/student/getStudentDetailsForSchool/{schoolname}",produces = MediaType.APPLICATION_JSON_VALUE)
-	public StudentAppData getStudents(@PathVariable String schoolname,@RequestHeader HttpHeaders headers) {			
-		logger.info("Reading Header Info ::::::::: {}",headers);		
-		String endUser=headers.getFirst("end-user") != null ? headers.getFirst("end-user") :"No End User";		
-		logger.info("Getting Student details for {} along with enduser ::::::::: {}",schoolname,endUser);				
-		StudentAppData appData=mongoDBStudentRepository.findByschoolname(schoolname);
+	@GetMapping(path = "/student/getStudentDetailsForSchool/{schoolname}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public StudentAppData getStudents(@PathVariable String schoolname, @RequestHeader HttpHeaders headers) {
+		logger.info("Reading Header Info ::::::::: {}", headers);
+		StudentAppData appData = null;	
+		if (headers.getFirst("end-user") == null || headers.getFirst("end-user").isEmpty()) {				
+			throw new IllegalArgumentException("The 'end user' parameter must not be null or empty");			
+		} else {				
+			logger.info("Getting Student details for {} along with enduser ::::::::: {}",schoolname,headers.getFirst("end-user"));				
+		}
+		appData = mongoDBStudentRepository.findByschoolname(schoolname);
 		return appData;
 	}
-	
-	@GetMapping(path="/student/all")
-    public @ResponseBody  List<Student> retriveStudent(@RequestHeader HttpHeaders headers) {
-		logger.info("Retrieving all students {} with headers.....",headers);	
-        List<Student> studentList = mongoDBStudentRepository.findAll();
-        return studentList;
-    }
-	
-	@PostMapping(path="/student/createstudent" )
-	public StudentAppData saveStudent(@RequestBody Student student) {	
-			logger.info("Posting Student details ................");			
-			StudentAppData appData=null;		
-			appData=mongoDBStudentRepository.save(student);				
+
+	@GetMapping(path = "/student/all")
+	public @ResponseBody List<Student> retriveStudent(@RequestHeader HttpHeaders headers) {
+		logger.info("Retrieving all students {} with headers.....", headers);
+		List<Student> studentList = mongoDBStudentRepository.findAll();
+		return studentList;
+	}
+
+	@PostMapping(path = "/student/createstudent")
+	public StudentAppData saveStudent(@RequestBody Student student) {
+		logger.info("Posting Student details ................");
+		StudentAppData appData = null;
+		appData = mongoDBStudentRepository.save(student);
 		return appData;
 	}
 }
