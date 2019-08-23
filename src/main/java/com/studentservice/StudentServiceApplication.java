d79f87f1832fa15730ba117ac96bc929fedcb87f
satvasu <satvasu@WKWIN8345480.global.publicisgroupe.net>
2019-08-23 16:32:21 +0530

    update opencensus

--- src/main/java/com/studentservice/StudentServiceApplication.java
+++ src/main/java/com/studentservice/StudentServiceApplication.java
@@ -1,24 +1,28 @@
 package com.studentservice;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.studentservice.util.TraceSample;
 
 @SpringBootApplication
 public class StudentServiceApplication {
 	
-	//private static final String PROJECT_ID = "sapient-si-dsst-184990";
+	private static final String PROJECT_ID = "sapient-si-dsst-184990";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
-		//try {
-		//	TraceSample.createAndRegisterGoogleCloudPlatform(PROJECT_ID);
-		//	TraceSample.createAndRegister();
-		//	TraceSample.doWork();
+		try {
+			TraceSample.createAndRegisterGoogleCloudPlatform(PROJECT_ID);
+			TraceSample.createAndRegister();
+			TraceSample.doWork();
 		
-		/*} catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 	
 	}
 
