7423fd6644e13cbdbc1f29e01caf61fe1505b968
satvasu <satvasu@WKWIN8345480.global.publicisgroupe.net>
2019-08-23 12:48:53 +0530

    Added logs

---
+++ src/main/java/com/studentservice/util/TraceSample.java
@@ -0,0 +1,81 @@
+package com.studentservice.util;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import io.opencensus.common.Scope;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
+import io.opencensus.trace.Tracer;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.samplers.Samplers;
+
+@Component
+public class TraceSample {
+	
+	private static final Tracer tracer = Tracing.getTracer();
+	
+	private static final String PROJECT_ID = "sapient-si-dsst-184990";
+
+
+	public static void doWork() {
+		// Create a child Span of the current Span.
+		try (Scope ss = tracer.spanBuilder("MyChildWorkSpan").startScopedSpan()) {
+			doInitialWork();
+			tracer.getCurrentSpan().addAnnotation("Finished initial work");
+			doFinalWork();
+		}
+	}
+
+	private static void doInitialWork() {
+		// ...
+		tracer.getCurrentSpan().addAnnotation("Doing initial work");
+		// ...
+	}
+
+	private static void doFinalWork() {
+		// ...
+		tracer.getCurrentSpan().addAnnotation("Hello Sathish!");
+		// ...
+	}
+	// [END trace_setup_java_custom_span]
+
+	// [START trace_setup_java_full_sampling]
+	public static void doWorkFullSampled() {
+		try (Scope ss = tracer.spanBuilder("MyChildWorkSpan").setSampler(Samplers.alwaysSample()).startScopedSpan()) {
+			doInitialWork();
+			tracer.getCurrentSpan().addAnnotation("Finished initial work");
+			doFinalWork();
+		}
+	}
+	// [END trace_setup_java_full_sampling]
+
+	// [START trace_setup_java_create_and_register]
+	public static void createAndRegister() throws IOException {
+		StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.builder().build());
+	}
+	// [END trace_setup_java_create_and_register]
+
+	// [START trace_setup_java_create_and_register_with_token]
+	/*
+	 * public static void createAndRegisterWithToken(String accessToken) throws
+	 * IOException { Date expirationTime = DateTime.now().plusSeconds(60).toDate();
+	 * 
+	 * GoogleCredentials credentials = GoogleCredentials.create(new
+	 * AccessToken(accessToken, expirationTime)); GoogleCredential credential =
+	 * GoogleCredential.getApplicationDefault();
+	 * 
+	 * StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.
+	 * builder()
+	 * .setProjectId("MyStackdriverProjectId").setCredentials(credentials).build());
+	 * }
+	 */
+	// [END trace_setup_java_create_and_register_with_token]
+
+	// [START trace_setup_java_register_exporter]
+	public static void createAndRegisterGoogleCloudPlatform(String projectId) throws IOException {
+		StackdriverTraceExporter
+				.createAndRegister(StackdriverTraceConfiguration.builder().setProjectId(PROJECT_ID).build());
+	}
+}
