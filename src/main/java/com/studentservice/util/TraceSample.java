57ae4cc7a65d4d7f54bb98c37f89d441cbd580ca
satvasu <satvasu@WKWIN8345480.global.publicisgroupe.net>
2019-08-23 16:56:15 +0530

    update

--- src/main/java/com/studentservice/util/TraceSample.java
+++ src/main/java/com/studentservice/util/TraceSample.java
@@ -1,65 +1,55 @@
-package com.studentservice.util;
-
-import java.io.IOException;
-
-import io.opencensus.common.Scope;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
-import io.opencensus.trace.Tracer;
-import io.opencensus.trace.Tracing;
-import io.opencensus.trace.samplers.Samplers;
-
-public class TraceSample {
-	private static final Tracer tracer = Tracing.getTracer();
-
-	public static void doWork() {
-		System.out.println("This is from doWork...........................");		
-		// Create a child Span of the current Span.
-		try (Scope ss = tracer.spanBuilder("MyChildWorkSpan").startScopedSpan()) {
-			doInitialWork();
-			tracer.getCurrentSpan().addAnnotation("Finished initial work");
-			doFinalWork();
-		}
-	}
-
-	private static void doInitialWork() {
-		// ...
-		tracer.getCurrentSpan().addAnnotation("Doing initial work");
-		// ...
-	}
-
-	private static void doFinalWork() {
-		// ...
-		tracer.getCurrentSpan().addAnnotation("Hello world!");
-		// ...
-	}
-	// [END trace_setup_java_custom_span]
-
-	// [START trace_setup_java_full_sampling]
-	public static void doWorkFullSampled() {
-		
-		System.out.println("This is from doWorkFullSampled...........................");
-		try (Scope ss = tracer.spanBuilder("MyChildWorkSpan").setSampler(Samplers.alwaysSample()).startScopedSpan()) {
-			doInitialWork();
-			tracer.getCurrentSpan().addAnnotation("Finished initial work");
-			doFinalWork();
-		}
-	}
-	// [END trace_setup_java_full_sampling]
-
-	// [START trace_setup_java_create_and_register]
-	public static void createAndRegister() throws IOException {
-		System.out.println("This is from createAndRegister...........................");
-		StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.builder().build());
-	}
-	// [END trace_setup_java_create_and_register]
-
-	
-	// [START trace_setup_java_register_exporter]
-	public static void createAndRegisterGoogleCloudPlatform(String projectId) throws IOException {
-		
-		System.out.println("This is from createAndRegisterGoogleCloudPlatform...........................");
-		StackdriverTraceExporter
-				.createAndRegister(StackdriverTraceConfiguration.builder().setProjectId(projectId).build());
-	}
-}
+/*
+ * package com.studentservice.util;
+ * 
+ * import java.io.IOException;
+ * 
+ * import io.opencensus.common.Scope; import
+ * io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
+ * import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
+ * import io.opencensus.trace.Tracer; import io.opencensus.trace.Tracing; import
+ * io.opencensus.trace.samplers.Samplers;
+ * 
+ * public class TraceSample { private static final Tracer tracer =
+ * Tracing.getTracer();
+ * 
+ * public static void doWork() {
+ * System.out.println("This is from doWork..........................."); //
+ * Create a child Span of the current Span. try (Scope ss =
+ * tracer.spanBuilder("MyChildWorkSpan").startScopedSpan()) { doInitialWork();
+ * tracer.getCurrentSpan().addAnnotation("Finished initial work");
+ * doFinalWork(); } }
+ * 
+ * private static void doInitialWork() { // ...
+ * tracer.getCurrentSpan().addAnnotation("Doing initial work"); // ... }
+ * 
+ * private static void doFinalWork() { // ...
+ * tracer.getCurrentSpan().addAnnotation("Hello world!"); // ... } // [END
+ * trace_setup_java_custom_span]
+ * 
+ * // [START trace_setup_java_full_sampling] public static void
+ * doWorkFullSampled() {
+ * 
+ * System.out.
+ * println("This is from doWorkFullSampled..........................."); try
+ * (Scope ss =
+ * tracer.spanBuilder("MyChildWorkSpan").setSampler(Samplers.alwaysSample()).
+ * startScopedSpan()) { doInitialWork();
+ * tracer.getCurrentSpan().addAnnotation("Finished initial work");
+ * doFinalWork(); } } // [END trace_setup_java_full_sampling]
+ * 
+ * // [START trace_setup_java_create_and_register] public static void
+ * createAndRegister() throws IOException { System.out.
+ * println("This is from createAndRegister...........................");
+ * StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.
+ * builder().build()); } // [END trace_setup_java_create_and_register]
+ * 
+ * 
+ * // [START trace_setup_java_register_exporter] public static void
+ * createAndRegisterGoogleCloudPlatform(String projectId) throws IOException {
+ * 
+ * System.out.
+ * println("This is from createAndRegisterGoogleCloudPlatform..........................."
+ * ); StackdriverTraceExporter
+ * .createAndRegister(StackdriverTraceConfiguration.builder().setProjectId(
+ * projectId).build()); } }
+ */
