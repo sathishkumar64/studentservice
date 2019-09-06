29a6cac8139f6a9bbe5b4f3c53ae1b229bfb21f8
sathishkumar64 <sathishkumar64@gmail.com>
2019-09-06 10:46:25 +0530

    revoked old code

--- src/main/java/com/studentservice/controller/StudentServiceController.java
+++ src/main/java/com/studentservice/controller/StudentServiceController.java
@@ -41,14 +41,11 @@ public class StudentServiceController {
 	@GetMapping(path = "/student/getStudentDetailsForSchool/{schoolname}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public StudentAppData getStudents(@PathVariable String schoolname, @RequestHeader HttpHeaders headers) {
 		logger.info("Reading Header Info ::::::::: {}", headers);
-		StudentAppData appData = null;	
-		if (headers.getFirst("end-user") == null || headers.getFirst("end-user").isEmpty()) {				
-			throw new IllegalArgumentException("The 'end user' parameter must not be null or empty");			
-		} else {				
-			logger.info("Getting Student details for {} along with enduser ::::::::: {}",schoolname,headers.getFirst("end-user"));				
-		}
-		appData = mongoDBStudentRepository.findByschoolname(schoolname);
-		return appData;
+ 		StudentAppData appData = null;	
+		String endUser=headers.getFirst("end-user") != null ? headers.getFirst("end-user") :"No End User";		
+		logger.info("Getting Student details for {} along with enduser ::::::::: {}",schoolname,endUser);				
+		appData=mongoDBStudentRepository.findByschoolname(schoolname);				
+ 		return appData;
 	}
 
 	@ApiOperation(value = "View a list of available students", response = Iterable.class)
